5e7ede68213a7708c7ddddfbbce6d07deb956d1e
Ramon Mora Marquez <60113219+rmora-marquez@users.noreply.github.com>
2021-08-03 18:34:59 -0500

    update delete usuarioapirest

--- src/main/java/edu/ieu/ejemplorest/api/UsuarioApiRest.java
+++ src/main/java/edu/ieu/ejemplorest/api/UsuarioApiRest.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,6 +44,7 @@ public class UsuarioApiRest {
         }
         return new ResponseEntity<Usuario>(user, HttpStatus.OK);
     }
+	
 	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Void> createUser(@RequestBody Usuario user){
 		System.out.println("Creating User " + user.getNombre());
@@ -53,4 +56,33 @@ public class UsuarioApiRest {
 		service.saveUser(user);
 		 return new ResponseEntity<Void>(HttpStatus.CREATED);
 	}
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Usuario> updateUser(@PathVariable("id") int id,
+			@RequestBody Usuario usuario){
+		 System.out.println("Updating User " + id);
+		 Usuario usuariobd = service.findById(id);
+		 if(usuariobd == null) {
+			 System.out.println("User with id " + id + " not found");
+			 return new ResponseEntity<Usuario>(HttpStatus.NOT_FOUND);
+		 }
+		 usuariobd.setApellidos( usuario.getApellidos() );
+		 usuariobd.setEmail( usuario.getEmail() );
+		 usuariobd.setNombre( usuario.getNombre() );
+		 usuariobd.setPassword( usuario.getPassword() );
+		 service.updateUser(usuariobd);
+		 return new ResponseEntity<Usuario>(usuariobd, HttpStatus.OK );
+	}
+	
+	@DeleteMapping(value="/{id}")
+	public ResponseEntity<Void> deleteUser(@PathVariable("id") int id){
+		System.out.println("Fetching & Deleting User with id " + id);
+		Usuario usuario = service.findById(id);
+		if(usuario == null) {
+			 System.out.println("Unable to delete. User with id " + id + " not found");
+			 return new ResponseEntity<Void>(HttpStatus.NOT_FOUND); // 404
+		}
+		service.deleteUserById(id);
+		return new ResponseEntity<Void>(HttpStatus.NO_CONTENT); // 204 http
+	}
+	
 }
